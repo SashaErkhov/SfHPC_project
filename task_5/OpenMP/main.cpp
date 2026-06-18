4287015698e8713af40815b9085b56098af2b6ce
Alexander Erkhov <sasori654654@gmail.com>
2026-06-18 16:50:21 +0300

    update task5

--- task_5/OpenMP/main.cpp
+++ task_5/OpenMP/main.cpp
@@ -1,48 +1,65 @@
 #include <iostream>
 #include <omp.h>
 #include <vector>
-#include <random>
-#include <cmath>
+#include <cstdlib>
+#include <limits>
 
-double foo(std::size_t total) {
-	std::size_t in = 0;
-	#pragma omp parallel reduction(+:in)
-	{
-		unsigned seed = 12345 + omp_get_thread_num();
-		// Обеспечивает разные последовательности случайных чисел у разных потоков
-		
-		std::mt19937 gen(seed);
-		std::uniform_real_distribution<double> dist(0.,1.);
+void foo(const std::vector<int>& arr, int& resMax, int& resCount) {
+  std::size_t n = arr.size();
+  if (n==0) {
+    resMax = 0;
+    resCount = 0;
+    return;
+  }
+  resMax = std::numeric_limits<int>::min();
+  resCount = 0;
+  
+  #pragma omp parallel
+  {
+    int localMax = std::numeric_limits<int>::min();
+    int localCount = 0;
+    
+    //параметр nowait отключает синхронизацию в конце цикла, сразу в блок critical
+    #pragma omp for nowait
+    for (std::size_t i = 0; i < n; ++i) {
+      if (arr[i] > localMax) {
+        localMax = arr[i];
+        localCount = 1;
+      } else if (arr[i] == localMax) {
+        ++localCount;
+      }
+    }
 
-		#pragma omp for
-		for(std::size_t i=0;i<total;++i) {
-			double x = dist(gen);
-			double y = dist(gen);
-			if (x*x + y*y <= 1) {
-				++in;
-			}
-		}
-	}
-	return 4. * static_cast<double>(in)/static_cast<double>(total);
+    #pragma omp critical
+    {
+      if (localMax > resMax) {
+        resMax = localMax;
+        resCount = localCount;
+      } else if (localMax == resMax) {
+        resCount += localCount;
+      }
+    }
+  }
 }
 
 int main() {
-	const double PI = 3.14159265358979323846;
-	std::size_t N = 10000;
-	while (N < 100000001) {
-		double time = 0.;
-		double pi = 0.;
-		double start = 0.;
-		double end = 0.;
-		for(std::size_t i=0;i<100;++i) {
-			start = omp_get_wtime();
-			pi = foo(N);
-			end = omp_get_wtime();
-			time += end-start;
-		}
-		time /= 100;
-		std::cout<<"N = "<<N<<" Time = "<<time<<" abs = "<<std::abs(pi-PI)<<std::endl;
-		N *= 10;
-	}
-	return 0;
+  for (std::size_t N = 10; N<=1000000; N*=10) {
+    std::vector<int> arr(N);
+    for (std::size_t i=0;i<N;++i) {
+      arr[i] = std::rand() % 100;
+    }
+    double time = 0.;
+    double start = 0.;
+    double end = 0.;
+    int res1, res2;
+    for (std::size_t i=0;i<100;++i) {
+      start = omp_get_wtime();
+      foo(arr, res1, res2);
+      end = omp_get_wtime();
+      time += end-start;
+    }
+    time /= 100;
+    std::cout << "| "<<N<<" | "<<time<<" |"<<std::endl;
+  }
+  return 0;
 }
